aa5315d087e94a62180b7adea719d0888e5fe4c3
Diego Czajka <diego.czajka@gmail.com>
2023-10-12 11:53:22 -0300

    feat openAPI swagger3

--- backend/src/main/java/com/diegoczajka/infra/exception/ErrorHandler.java
+++ backend/src/main/java/com/diegoczajka/infra/exception/ErrorHandler.java
@@ -2,35 +2,51 @@ package com.diegoczajka.infra.exception;
 
 import com.diegoczajka.domain.ValidationException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity error404() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity error400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors();
 
-        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+        return ResponseEntity.badRequest().body(erros.stream().map(error ->
+                        new ValidationError(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList()));
     }
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity unprocessableEntity(ValidationException ex) {
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity BAD_REQUEST(ValidationException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
-    private record DadosErroValidacao(String campo, String mensagem) {
+    private class ValidationError {
+        private String field;
+        private String message;
 
-        public DadosErroValidacao(FieldError erro) {
-            this(erro.getField(), erro.getDefaultMessage());
+        public ValidationError(String field, String message) {
+            this.field = field;
+            this.message = message;
+        }
+
+        public ValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
         }
 
     }
